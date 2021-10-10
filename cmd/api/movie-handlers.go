8a8ed3202cac272d1bc178add7c5fb248c7c80bc
Christopher Lim <christopher@supermpower.com>
2021-10-11 00:01:26 +0800

    update

---
+++ cmd/api/movie-handlers.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+	"strconv"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
+	params := httprouter.ParamsFromContext(r.Context())
+
+	id, err := strconv.Atoi(params.ByName("id"))
+	if err != nil {
+		app.logger.Println(errors.New("invalid ID parameter"))
+		app.errorJSON(w, err)
+		return
+	}
+
+	app.logger.Println("ID : ", id)
+
+	movie, err := app.models.DB.Get(id)
+
+	err = app.writeJSON(w, http.StatusOK, movie, "movie")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, movies, "movie")
+
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
